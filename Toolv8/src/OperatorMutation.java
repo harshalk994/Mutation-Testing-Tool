24b6bea608f7d8a7f71cbf30218de02350b10474
Harshal <harshalkasle1994@gmail.com>
2020-06-04 16:33:45 +0100

    Adding the OperatorMutation Class

---
+++ Toolv8/src/OperatorMutation.java
@@ -0,0 +1,92 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperatorMutation {
+	
+	
+//	public static void main(String[] args) throws IOException {
+//		List<String> arithOpList = new ArrayList<String>();
+//		List<String> conditionalOpList = new ArrayList<String>();
+//		List<String> relationalOpList = new ArrayList<String>();
+//		
+//		OperatorStorage ops = new OperatorStorage();
+//		ops.processOp();
+//		
+//		ConditionalOpStorage cs = new ConditionalOpStorage();
+//		cs.processOp();
+//		
+//		RelOpStorageFinal rs = new RelOpStorageFinal();
+//		rs.processOp();
+//		
+//		ArithOpTry3 aop = new ArithOpTry3();
+//		ConditionOpTry2 cop = new ConditionOpTry2();
+//		RelOpTryFinal rop = new RelOpTryFinal();
+//		
+//		arithOpList = ops.returnOpList();
+//		System.out.println("ArithList");
+//		for(int i=0;i<arithOpList.size();i++) {
+//			System.out.println(arithOpList.get(i));
+//		}
+////		if(arithOpList.isEmpty() == false) {
+////			aop.generateArithOpMutantFiles();
+////		}
+//		
+//		conditionalOpList = cs.returnOpList();
+//		System.out.println("ConditionList");
+//		for(int i=0;i<conditionalOpList.size();i++) {
+//			System.out.println(conditionalOpList.get(i));
+//		}
+////		if(conditionalOpList.isEmpty() == false) {
+////			cop.generateConditionMutantFiles();
+////		}
+//		
+//		relationalOpList = rs.returnRelOpList();
+//		System.out.println("RelList");
+//		for(int i=0;i<relationalOpList.size();i++) {
+//			System.out.println(relationalOpList.get(i));
+//		}
+////		if(relationalOpList.isEmpty() == false) {
+////			rop.generateRelOpMutantFiles();
+////		}
+//	
+//
+//	}
+	
+	public void opMutationRules() throws IOException {
+		List<String> arithOpList = new ArrayList<String>();
+		List<String> conditionalOpList = new ArrayList<String>();
+		List<String> relationalOpList = new ArrayList<String>();
+		
+		OperatorStorage ops = new OperatorStorage();
+		ops.processOp();
+		arithOpList = ops.returnOpList();
+		
+		ConditionalOpStorage cs = new ConditionalOpStorage();
+		cs.processOp();
+		conditionalOpList = cs.returnOpList();
+		
+		RelOpStorageFinal rs = new RelOpStorageFinal();
+		rs.processOp();
+		relationalOpList = rs.returnRelOpList();
+	
+		
+		if(arithOpList.isEmpty() == false) {
+			ArithOpTry3 aop = new ArithOpTry3();
+			aop.generateArithOpMutantFiles();
+		}
+		
+
+		if(conditionalOpList.isEmpty() == false) {
+			ConditionOpTry2 cop = new ConditionOpTry2();
+			cop.generateConditionMutantFiles();
+		}
+		
+		
+		if(relationalOpList.isEmpty() == false) {
+			RelOpTryFinal rop = new RelOpTryFinal();
+			rop.generateRelOpMutantFiles();
+		}
+	}
+
+}
