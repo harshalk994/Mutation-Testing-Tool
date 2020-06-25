9d288bbf31c5c27a1c4473748de53d262c3406a5
Harshal <harshalkasle1994@gmail.com>
2020-06-25 16:56:01 +0100

    Added code to support programs with package structure

--- Toolv8/src/UserInputs.java
+++ Toolv8/src/UserInputs.java
@@ -1,11 +1,16 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class UserInputs {
 	
 	String originalpath;
 	String mutantpath;
+	String finalPath;
 	
 	public void readProperties() {
 		File configFile = new File("config.properties");
@@ -17,15 +22,42 @@ public class UserInputs {
 		 
 		    String oPath = props.getProperty("originalprogrampath");
 		    String mPath = props.getProperty("mutantdestination");
-		    setProperties(oPath, mPath);
+		    String pName = props.getProperty("packagename");
+		    setProperties(oPath, mPath, pName);
 		} catch (Exception e) {
 		    e.printStackTrace();
 		}
 	}
 	
-	public void setProperties(String oPath, String mPath) {
+	public void setProperties(String oPath, String mPath, String pName) throws IOException {
 		originalpath = oPath;
 		mutantpath = mPath;
+		String appendPath;
+		if(pName!=null) {
+			if(pName.contains(".")) {
+				appendPath = pName.replace(".", "\\");
+				mutantpath = mPath+"\\"+appendPath;
+				Path path = Paths.get(mutantpath);
+				
+				if(!Files.exists(path)){
+					Files.createDirectories(path);
+					System.out.println("Driectory structure created");
+				}else {
+					System.out.println("Directory already exists");
+				}
+			}else {
+				mutantpath = mPath+"\\"+pName;
+				Path path = Paths.get(mutantpath);
+				
+				if(!Files.exists(path)){
+					Files.createDirectories(path);
+					System.out.println("Driectory structure created");
+				}else {
+					System.out.println("Directory already exists");
+				}
+			}
+		}
+		
 	}
 	
 	public String returnOPath() {
