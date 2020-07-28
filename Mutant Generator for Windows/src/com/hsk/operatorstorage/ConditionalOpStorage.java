f57155123b702b2c0c07553e2be9307b971ee4dc
Harshal <harshalkasle1994@gmail.com>
2020-07-28 16:18:28 +0100

    Added Apache license 2.0 for Mutant Generator Windows source code

--- Mutant Generator for Windows/src/com/hsk/operatorstorage/ConditionalOpStorage.java
+++ Mutant Generator for Windows/src/com/hsk/operatorstorage/ConditionalOpStorage.java
@@ -1,4 +1,19 @@
-//	This class is created to store conditional operators found in the original class
+/* 
+	   Copyright 2020 Dr. Klaas-Jan Stol, Harshal Kasle
+	
+	   Licensed under the Apache License, Version 2.0 (the "License");
+	   you may not use this file except in compliance with the License.
+	   You may obtain a copy of the License at
+	
+	       http://www.apache.org/licenses/LICENSE-2.0
+	
+	   Unless required by applicable law or agreed to in writing, software
+	   distributed under the License is distributed on an "AS IS" BASIS,
+	   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	   See the License for the specific language governing permissions and
+	   limitations under the License.
+ */
+
 
 package com.hsk.operatorstorage;
 import com.hsk.*;
@@ -10,6 +25,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+//This class is created to store conditional operators found in the original class
 public class ConditionalOpStorage {
 	private static List<String> conditionOpL = new ArrayList<String>();
 	private static List<String> conditionOpP = new ArrayList<String>();
